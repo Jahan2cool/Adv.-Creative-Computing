fc0808ae25b6e0b40c959027869a9d1d210f2f85
Sleepy <56746603+Jahan2cool@users.noreply.github.com>
2023-12-05 23:18:26 -0800

    a

--- Diamond/elegant 2.py
+++
@@ -1,4 +0,0 @@
-for x in range(9):
-    for y in range(abs(4-x)): print(" ",end = "")
-    for y in range(5-abs(4-x)): print("@@", end = "")
-    print()
--- Diamond/elegant.py
+++ Diamond/elegant.py
@@ -1,4 +1,4 @@
-for x in range(-10, 11, 2):
-    for y in range(abs(x)//2): print(" ",end = "")
-    for y in range(10-abs(x)): print("@", end = "")
+for x in range(9):
+    for y in range(abs(4-x)): print(" ",end = "")
+    for y in range(5-abs(4-x)): print("@@", end = "")
     print()
